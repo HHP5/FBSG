6ea439bba8fa987d49ada8aac194c0de7f7dd201
Ekaterina <ekaterina_grigoryeva97@mail.ru>
2021-01-06 16:17:39 +0700

    Add new functions

--- FilmBookSerial/Controller/CategoryViewController.swift
+++ FilmBookSerial/Controller/CategoryViewController.swift
@@ -9,96 +9,150 @@ import UIKit
 import CoreData
 
 class CategoryViewController: UITableViewController {
-    
-    var categories = ["Films","Books","Serials","Games"]
+
+    var categories = [Category]()
     var itemArray = [Items]()
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
         print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
+        
+        loadCategories()
+        
+        // первоначальное создание фиксированный категорий
+        if categories.count == 0 {
+            createCategories()
+        }
+
     }
-    
-    // MARK: - Table view data source
-    
+
+    // для обновления числа кол-ва заметок в категории при возврате на главный экран (если заметка удалена)
+    override func viewDidAppear(_ animated: Bool) {
+        loadCategories()
+    }
+
+
+    // MARK: - TableView dataSource
+
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return categories.count
     }
-    
+
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
+
         let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath)
-        cell.textLabel?.text = categories[indexPath.row]
+        let category = categories[indexPath.row]
+        cell.textLabel?.text = category.name              
+        cell.detailTextLabel?.text = "\(category.quantity)" //кол-во заметок в категории
         return cell
     }
-    
+
+    //MARK: - Добавление новых элементов
+
     @IBAction func addItemButton(_ sender: UIBarButtonItem) {
-        
+
         var textField = UITextField()
-        
+
         let alert = UIAlertController(title: "Add new Item", message: "", preferredStyle: .alert)
-        
+
         var action = [UIAlertAction]()
-        for i in 0...categories.count-1{
-            action.append(UIAlertAction(title: categories[i], style: .default){[self] (action) in
+        for i in 0...categories.count - 1 {
+            action.append(UIAlertAction(title: categories[i].name, style: .default) { [self] (action) in
                 let newItem = Items(context: self.context)
                 newItem.title = textField.text!
-                newItem.category = categories[i]
+                newItem.category = categories[i].name
                 newItem.status = false
                 self.itemArray.append(newItem)
+                categories[i].quantity += 1
                 self.saveItems()
             })
             alert.addAction(action[i])
         }
-        
+
         let cancel = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
-        
+
         alert.addTextField { (alertText) in
             alertText.placeholder = "Create New Item"
             textField = alertText
             print(alertText)
         }
-        
+
         alert.addAction(cancel)
         present(alert, animated: true)
     }
-    
+
+    //MARK: - Table View Delegate Methods
+
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         performSegue(withIdentifier: "toItems", sender: self)
-        print("\(categories[indexPath.row]) njbbjbmj" )
     }
-    
+
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         let destinationVC = segue.destination as! ItemViewController
-        if let indexPath = tableView.indexPathForSelectedRow{
-            print(categories[indexPath.row])
+        if let indexPath = tableView.indexPathForSelectedRow {
             destinationVC.selectedCategory = categories[indexPath.row]
         }
     }
-    
-    func saveItems(){
-        do{
+
+    //MARK: - Методы CoreData
+
+    func saveItems() {
+        do {
             try context.save()
-        }catch{
+        } catch {
             print("Error saving context \(error)")
         }
+        tableView.reloadData()
     }
-    
+
     func loadItems(with request: NSFetchRequest<Items> = Items.fetchRequest()) {
-        do{
+        do {
             itemArray = try context.fetch(request)
-        }catch{
+        } catch {
             print("Error fetching request \(error)")
         }
         tableView.reloadData()
     }
-    
-    func deleteAllData(entity: String)
-    {
+
+    func loadCategories(with request: NSFetchRequest<Category> = Category.fetchRequest()) {
+
+        do {
+            categories = try context.fetch(request)
+        } catch {
+            print("Error fetching data from context \(error)")
+        }
+        tableView.reloadData()
+    }
+
+    func createCategories() {
+        
+        createCategory(name: "Films")
+        createCategory(name: "Books")
+        createCategory(name: "Serials")
+        createCategory(name: "Games")
+        saveItems()
+    }
+
+    func createCategory(name: String) {
+        
+        let newCategory = Category(context: self.context)
+        newCategory.name = name
+        newCategory.quantity = 0
+        categories.append(newCategory)
+    }
+
+
+    func deleteAllData(entity: String){
+        
         let ReqVar = NSFetchRequest<NSFetchRequestResult>(entityName: entity)
         let DelAllReqVar = NSBatchDeleteRequest(fetchRequest: ReqVar)
-        do { try context.execute(DelAllReqVar) }
-        catch { print(error) }
+        do {
+            try context.execute(DelAllReqVar)
+        } catch {
+            print(error)
+        }
     }
+
 }
--- FilmBookSerial/Controller/ItemViewController.swift
+++ FilmBookSerial/Controller/ItemViewController.swift
@@ -9,99 +9,131 @@ import UIKit
 import CoreData
 
 class ItemViewController: UITableViewController {
-    
+
     var itemArray: [Items]?
-    
-    var selectedCategory: String?
+    var quantityOfItems: Int?
+    var selectedCategory: Category? {
+        didSet {
+            loadItems()
+        }
+    }
     var context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
         loadItems()
-        title = selectedCategory
+
+        if let category = selectedCategory {
+            title = category.name
+        }
     }
-    
-    // MARK: - Table view data source
-    
+
+    // MARK: - TableView dataSource
+
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray?.count ?? 0
     }
-    
+
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ItemCell", for: indexPath)
         let item = itemArray?[indexPath.row]
         cell.textLabel?.text = item!.title
         cell.accessoryType = item!.status ? .checkmark : .none //для постановки галочки или снятия (при повторном нажатии)
-        
+
         return cell
     }
-    
+
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        if let item = itemArray{
+        if let item = itemArray {
             item[indexPath.row].status = !item[indexPath.row].status
             saveItems()
             tableView.deselectRow(at: indexPath, animated: true)
         }
     }
-    
+
+    //MARK: - SWIPE влево (редактирование и удаление)
+
     override func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
-        
+
         let deleteAction = UIContextualAction(style: .destructive, title: "Удалить") { (action, view, handler) in
             self.context.delete(self.itemArray![indexPath.row])
             self.itemArray?.remove(at: indexPath.row)
+            self.selectedCategory?.quantity -= 1
             self.saveItems()
+
         }
         deleteAction.image = UIImage(systemName: "trash")
-        
+
         let editAction = UIContextualAction(style: .normal, title: "Редактировать") { (action, view, handler) in
             var textField = UITextField()
             let alert = UIAlertController(title: "Change", message: "", preferredStyle: .alert)
-            
-            let alertAction = UIAlertAction(title: "Save", style: .default){[self] (action) in
+
+            let alertAction = UIAlertAction(title: "Save", style: .default) { [self] (action) in
                 itemArray?[indexPath.row].setValue(textField.text!, forKey: "title")
                 self.saveItems()
             }
             let cancel = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
-            
+
             alert.addTextField { (alertText) in
-                alertText.placeholder = "Change Item"
+                if let text = self.itemArray?[indexPath.row].title{
+                    alertText.text = "\(text)"
+                }
                 textField = alertText
                 print(alertText)
             }
-            
+
             alert.addAction(cancel)
             alert.addAction(alertAction)
-            
+
             self.present(alert, animated: true)
-            
+
             self.saveItems()
         }
-        
+
         editAction.image = UIImage(systemName: "square.and.pencil")
-        
-        let configuration = UISwipeActionsConfiguration(actions: [deleteAction,editAction])
+
+        let configuration = UISwipeActionsConfiguration(actions: [deleteAction, editAction])
         configuration.performsFirstActionWithFullSwipe = false
         return configuration
     }
+
+    //MARK: - SWIPE вправо (поделиться)
+
+    override func tableView(_ tableView: UITableView, leadingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+
+        let shareAction = UIContextualAction(style: .normal, title: "Поделиться") { (action, view, handler) in
+            let textToShare: [Any] = [self.itemArray![indexPath.row].title!]
+            let activityViewController = UIActivityViewController(activityItems: textToShare, applicationActivities: nil)
+            activityViewController.popoverPresentationController?.sourceView = self.view
+            self.present(activityViewController, animated: true)
+
+        }
+        shareAction.image = UIImage(systemName: "square.and.arrow.up")
+        let configuration = UISwipeActionsConfiguration(actions: [shareAction])
+        configuration.performsFirstActionWithFullSwipe = false
+        return configuration
+    }
+
+    //MARK: - CRUD
     
-    func saveItems(){
-        do{
+    func saveItems() {
+        do {
             try context.save()
-        }catch{
+        } catch {
             print("Error saving items \(error)")
         }
         tableView.reloadData()
     }
-    
-    func loadItems(with request: NSFetchRequest<Items> = Items.fetchRequest(), predicate: NSPredicate? = nil){
-        
-        let categoryPredicate = NSPredicate(format: "category MATCHES %@", selectedCategory!)
+
+    func loadItems(with request: NSFetchRequest<Items> = Items.fetchRequest()) {
+
+        let categoryPredicate = NSPredicate(format: "category MATCHES %@", selectedCategory!.name!)
         request.predicate = categoryPredicate
-        
-        do{
+
+        do {
             itemArray = try context.fetch(request)
-        }catch{
+        } catch {
             print("Error fetching data from context \(error)")
         }
         tableView.reloadData()
